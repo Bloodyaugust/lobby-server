3434e89f5e6c72fee428a920aad2383eb5092503
Bloodyaugust <greysonrichey@gmail.com>
2020-10-28 00:06:04 -0600

    Update ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,11 +11,11 @@ module.exports = {
   deploy : {
     production : {
       user : 'root',
-      host : '192.81.135.1',
+      host : '192.81.135.83',
       ref  : 'origin/master',
       repo : 'git@github.com:Bloodyaugust/lobby-server.git',
       path : '/var/www/lobby-server',
-      key  : '~/.ssh/deploy_rsa.pub',
+      key  : '~/.ssh/lobby_server_deploy_rsa.pub',
       'pre-deploy-local': '',
       'post-deploy' : 'yarn install && pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
