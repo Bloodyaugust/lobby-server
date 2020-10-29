06434c6f83db6f9dc11dbb66ad81715997ef772f
Bloodyaugust <greysonrichey@gmail.com>
2020-10-29 01:03:35 -0600

    Fix ecosystem linting

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,21 +1,21 @@
 module.exports = {
-  apps : [{
+  apps: [{
     name: 'Lobby Server',
     script: 'index.js',
     watch: '.'
   }],
 
-  deploy : {
-    production : {
-      user : 'root',
-      host : '192.81.135.83',
-      ref  : 'origin/master',
-      repo : 'git@github.com:Bloodyaugust/lobby-server.git',
-      path : '/var/www/lobby-server',
-      key  : '~/.ssh/lobby_server_deploy2_rsa.pem',
+  deploy: {
+    production: {
+      user: 'root',
+      host: '192.81.135.83',
+      ref: 'origin/master',
+      repo: 'git@github.com:Bloodyaugust/lobby-server.git',
+      path: '/var/www/lobby-server',
+      key: '~/.ssh/lobby_server_deploy2_rsa.pem',
       'pre-deploy-local': '',
-      'post-deploy' : 'yarn install && mv .env.prod .env && pm2 startOrReload ecosystem.config.js --env production',
+      'post-deploy': 'yarn install && mv .env.prod .env && pm2 startOrReload ecosystem.config.js --env production',
       'pre-setup': ''
     }
   }
-};
+}
