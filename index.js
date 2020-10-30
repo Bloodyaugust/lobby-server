24a6ed532e2cdc7167953361df0a891df7a92e05
Bloodyaugust <greysonrichey@gmail.com>
2020-10-30 03:51:24 -0600

    Separate DGRAM server to discover and lobby

--- index.js
+++ index.js
@@ -7,13 +7,19 @@ const express = require('express')
 const rateLimit = require('express-rate-limit')
 
 const app = express()
-const dgramServer = dgram.createSocket('udp4')
+const discoverServer = dgram.createSocket('udp4')
+const lobbyServer = dgram.createSocket('udp4')
 const lobbyNameLength = 1 * 2
 const lobbyKeyLength = 4 * 4
 const lobbyTimeout = [parseInt(process.env.LOBBY_TIMEOUT_SECONDS), 'second']
+const peerIDLength = 4 * 4
+const peerTimeout = [parseInt(process.env.PEER_TIMEOUT_SECONDS), 'second']
 const port = process.env.HOST_PORT
 
 const lobbyTimeoutInterval = dayjs().add(...lobbyTimeout).valueOf() - dayjs().valueOf()
+const peerTimeoutInterval = dayjs().add(...peerTimeout).valueOf() - dayjs().valueOf()
+
+let exitCode = 0
 
 const getLobbyLimiter = rateLimit({
   windowMs: 60 * 1000,
@@ -32,6 +38,7 @@ const rootLimiter = rateLimit({
   max: process.env.NODE_ENV !== 'production' ? 9999 : 4
 })
 
+let udpPeers = []
 let lobbies = []
 
 function formatLobby (lobby, preserveKey) {
@@ -63,6 +70,28 @@ function lobbyFactory (req) {
     key: crypto.randomBytes(lobbyKeyLength).toString('hex'),
     name: lobbyName,
     private: req.query.private === 'true'
+  }
+}
+
+function udpPeerFactory (peerData) {
+  const now = dayjs()
+
+  let peerID = crypto.randomBytes(peerIDLength).toString('hex').toUpperCase()
+
+  while (udpPeers.find(peer => peer.id === peerID)) {
+    peerID = crypto.randomBytes(peerIDLength).toString('hex').toUpperCase()
+  }
+
+  return {
+    address: peerData.address,
+    created: now,
+    expires: now.add(...peerTimeout),
+    host: null,
+    id: peerID,
+    lobby: null,
+    lobbyAddress: null,
+    lobbyPort: null,
+    port: peerData.port
   }
 }
 
@@ -115,21 +144,114 @@ app.post('/lobby/:name', (req, res) => {
   res.json(formatLobby(lobby))
 })
 
-dgramServer.on('error', (err) => {
-  console.log(`DGRAM Server error: ${err}`)
-  dgramServer.close()
+discoverServer.on('error', (err) => {
+  console.log(`Discover Server error: ${err}`)
+  exitCode = 1
+  process.kill(process.pid, 'SIGINT')
+})
+lobbyServer.on('error', (err) => {
+  console.log(`Lobby Server error: ${err}`)
+  exitCode = 1
+  process.kill(process.pid, 'SIGINT')
 })
 
-dgramServer.on('message', (msg, rinfo) => {
-  console.log(`DGRAM message received: ${msg} with rinfo: ${JSON.stringify(rinfo)}`)
-  dgramServer.send('test back', rinfo.port, rinfo.address)
+discoverServer.on('message', (msg, rinfo) => {
+  const jsonMessage = JSON.parse(decodeURIComponent(escape(msg)))
+  let newPeer
+
+  console.log(`DISCOVER message received: ${JSON.stringify(jsonMessage)} with rinfo: ${JSON.stringify(rinfo)}`)
+
+  switch (jsonMessage.type) {
+    case 'DISCOVER':
+      newPeer = udpPeerFactory({
+        ...rinfo
+      })
+
+      discoverServer.send(unescape(encodeURIComponent(JSON.stringify({
+        type: 'ACK_DISCOVER',
+        peer: newPeer
+      }))), newPeer.port, newPeer.address)
+
+      udpPeers.push(newPeer)
+      break
+
+    default:
+      break
+  }
+})
+lobbyServer.on('message', (msg, rinfo) => {
+  const jsonMessage = JSON.parse(decodeURIComponent(escape(msg)))
+  const gamePeers = udpPeers.filter(peer => peer.lobby === jsonMessage.lobby)
+  let currentPeer, updatingPeer
+
+  console.log(`LOBBY message received: ${JSON.stringify(jsonMessage)} with rinfo: ${JSON.stringify(rinfo)}`)
+
+  switch (jsonMessage.type) {
+    case 'JOIN':
+      updatingPeer = udpPeers.find(peer => peer.id === jsonMessage.peerID)
+
+      updatingPeer.lobbyAddress = rinfo.address
+      updatingPeer.lobbyPort = rinfo.port
+
+      lobbyServer.send(unescape(encodeURIComponent(JSON.stringify({
+        type: 'ACK_JOIN',
+        peer: updatingPeer
+      }))), updatingPeer.lobbyPort, updatingPeer.lobbyAddress)
+      console.log('sent ack join')
+
+      gamePeers.forEach((peer) => {
+        lobbyServer.send(unescape(encodeURIComponent(JSON.stringify({
+          type: 'JOIN',
+          peer: updatingPeer
+        }))), peer.lobbyPort, peer.lobbyAddress)
+
+        lobbyServer.send(unescape(encodeURIComponent(JSON.stringify({
+          type: 'JOIN',
+          peer: peer
+        }))), updatingPeer.lobbyPort, updatingPeer.lobbyAddress)
+      })
+      break
+
+    case 'START':
+      gamePeers.forEach((peer) => {
+        lobbyServer.send(unescape(encodeURIComponent(JSON.stringify({
+          type: 'START'
+        }))), peer.lobbyPort, peer.lobbyAddress)
+      })
+      break
+
+    case 'KEEPALIVE':
+      currentPeer = udpPeers.find(peer => peer.id === jsonMessage.peerID)
+
+      currentPeer.expires = dayjs().add(...peerTimeout)
+
+      lobbyServer.send(unescape(encodeURIComponent(JSON.stringify({
+        type: 'ACK'
+      }))), currentPeer.lobbyPort, currentPeer.lobbyAddress)
+      break
+
+    case 'LEAVE':
+      currentPeer = udpPeers.splice(udpPeers.findIndex(peer => peer.id === jsonMessage.peerID), 1)
+
+      lobbyServer.send(unescape(encodeURIComponent(JSON.stringify({
+        type: 'ACK'
+      }))), currentPeer.lobbyPort, currentPeer.lobbyAddress)
+      break
+
+    default:
+      break
+  }
 })
 
-dgramServer.on('listening', () => {
-  console.log(`DGRAM server listening: ${JSON.stringify(dgramServer.address())}`)
+discoverServer.on('listening', () => {
+  console.log(`DISCOVER server listening: ${JSON.stringify(discoverServer.address())}`)
+})
+lobbyServer.on('listening', () => {
+  console.log(`LOBBY server listening: ${JSON.stringify(lobbyServer.address())}`)
 })
 
-dgramServer.bind(31401)
+discoverServer.bind(process.env.DISCOVER_HOST_PORT)
+lobbyServer.bind(process.env.LOBBY_HOST_PORT)
 
 const server = app.listen(port, () => {
   process.send('ready')
@@ -144,13 +266,22 @@ const cleanupLobbiesInterval = setInterval(() => {
   console.log(`Removed ${initialLobbyLength - lobbies.length}/${initialLobbyLength} lobbies. ${lobbies.length} lobbies currently active.`)
 }, lobbyTimeoutInterval)
 
+const cleanupPeersInterval = setInterval(() => {
+  const initialUDPPeersLength = udpPeers.length
+
+  udpPeers = udpPeers.filter(peer => peer.expires.isAfter(dayjs()))
+  console.log(`Removed ${initialUDPPeersLength - udpPeers.length}/${initialUDPPeersLength} udpPeers. ${udpPeers.length} udpPeers currently active.`)
+}, peerTimeoutInterval)
+
 process.on('SIGINT', () => {
   clearInterval(cleanupLobbiesInterval)
+  clearInterval(cleanupPeersInterval)
 
-  dgramServer.close()
+  discoverServer.close()
+  lobbyServer.close()
 
   server.close(() => {
     console.log('Received SIGINT: Lobby Server shutting down...')
-    process.exit(0)
+    process.exit(exitCode)
   })
 })
